b720e109ba502fc917b253bc3846fd059e49b6a9
Ryan <pinkstonr54@gmail.com>
2023-12-27 23:43:43 -0600

    added bootstrap framework and custom css

--- app/page.js
+++ app/page.js
@@ -5,14 +5,21 @@
 
 
 import BtnGenerator from './BtnGenerator';
+import { useEffect } from 'react';
+
+
 
 export default function Home() {
+
+  useEffect(()=>{
+    import("bootstrap/dist/js/bootstrap");
+},[])
 
 
   return (
     <>
-      <h1>Let's do basic arithmetic!</h1>
-      <p>It currently does not do order of operations</p>
+      <h1 className='title'>Let's do basic arithmetic!</h1>
+      <p className='instructions'>It currently does not do order of operations</p>
       <BtnGenerator />
     </>
   )
